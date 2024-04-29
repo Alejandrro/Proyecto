03d076ae4b832eecce1d1846aef361f6eb165cd6
Alejandro <allejandro_15@hotmail.com>
2024-04-28 18:30:24 -0600

    agregando menu

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.scss';
-import NavBar from './components/NavBar';
-import SideBar from './components/SideBar';
 import Home from './pages/Home';
 import Ventas from './pages/Ventas';
 import Clientes from './pages/Clientes';
+import ColorSchemesExample from './components/NavBar';
 
 function App() {
   return (
-    <Router className="App">
+    <Router>
+      <ColorSchemesExample />
+      <Routes>
+        <Route path="/" exact={true} Component={Home} />
+        <Route path="/ventas" exact={true} Component={Ventas} />
+        <Route path="/clientes" exact={true} Component={Clientes} />
+      </Routes>
+    </Router>
+    /*<Router className="App">
       <NavBar />
       <div className='flex'>
         <SideBar />
@@ -20,7 +27,7 @@ function App() {
           </Routes>
         </div>
       </div>
-    </Router>
+    </Router>*/
   );
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,29 @@
-const Navbar = () => {
-    return (
-        <div className="navbar">
-            NavBar
-        </div>
-    );
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
+
+function ColorSchemesExample() {
+  return (
+    <>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">ProgramarKeting</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link>
+              <Link to="/">Inicio</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link to="/ventas">Ventas</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link to="/clientes">Clientes</Link>
+            </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
 }
 
-export default Navbar;
+export default ColorSchemesExample;
